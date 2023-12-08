d62e2cea9913ce811fa4dc68397c8c145bf07ed4
grot4486 <115872950+grot4486@users.noreply.github.com>
2023-12-08 14:59:35 -0500

    Add files via upload

---
+++ inkapsylacia.py
@@ -0,0 +1,39 @@
+class Orc:
+    def __init__(self, n, a, h):
+        print('Доступен объект класса Orc')
+        self.name=n
+        self.age=a
+        self.height=h
+    
+    def print(self):
+        print(f'Введите имя:{self.name}')
+        print(f'Введите возраст:{self.age}')
+        print(f'Введите рост:{self.height}')
+     
+    @property
+    def name(self):
+        return self.__name.upper()
+        
+    @name.setter    
+    def name(self, n):
+        self.__name=n.capitalize()
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if 0 < age < 300:
+            self.__age = age
+        else:
+            print("Данный обьект уже мертв")
+
+
+class Destroyer(Orc):
+    def study(self):
+        print(f'{self.name} Всё крушит')
+            
+
+person1=Student('Maxim', 100, 189)
+person1.print()
+person1.study()
